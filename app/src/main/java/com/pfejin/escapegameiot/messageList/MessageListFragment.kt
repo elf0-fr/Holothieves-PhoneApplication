0650a05fc93de9bd0b5d3c21bb452033a4e9cc71
Romain Tirbisch <elf0@macbook-pro-de-romain.home>
2020-11-25 09:39:11 +0100

    messageFragment

---
+++ app/src/main/java/com/pfejin/escapegameiot/messageList/MessageListFragment.kt
@@ -0,0 +1,18 @@
+package com.pfejin.escapegameiot.messageList
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.pfejin.escapegameiot.R
+
+class MessageListFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_message_list, container, false)
+    }
+}
